899560770248f4b468a219d051b9c5d79b113ce9
Edward Kang <eyk23@cornell.edu>
2014-02-19 10:30:26 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TunecoreProject::Application.config.secret_token = 'c748aca6b8c29a2537fa89e3bdb7c6bed52008bc6623d0caf1b1974594f1c57f6cad670c3d5b9255e782d0fdd3489e096486df1d3dfa769726eeaaf9fd0a0089'
